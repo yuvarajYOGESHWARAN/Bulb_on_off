1414c301a5a06be0bd83226bcf45ac36054f5d45
yuvarajYOGESHWARAN <132157657+yuvarajYOGESHWARAN@users.noreply.github.com>
2023-07-12 11:09:32 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+var on_btn = document.getElementById("on");
+var off_btn = document.getElementById("off");
+var img = document.getElementById("bulb");
+function bulb_on() {
+    img.src="/bulbon.webp"
+}
+function bulb_off(){
+    img.src="/bulboff.png"
+}
