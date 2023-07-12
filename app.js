20741fdbc15630459b18f3a8e4f24b197c4bea74
yuvarajYOGESHWARAN <132157657+yuvarajYOGESHWARAN@users.noreply.github.com>
2023-07-12 11:11:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var on_btn = document.getElementById("on");
 var off_btn = document.getElementById("off");
 var img = document.getElementById("bulb");
 function bulb_on() {
-    img.src="/bulbon.webp"
+    img.src="bulbon.webp"
 }
 function bulb_off(){
-    img.src="/bulboff.png"
+    img.src="bulboff.png"
 }
